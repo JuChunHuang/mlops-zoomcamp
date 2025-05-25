2674708ef86028391377cdc2dae7b31644e03576
Ju-Chun Huang <ivyhuang1997@gmail.com>
2025-05-24 23:02:20 -0400

    ad hw2

--- cohorts/2025/02-experiment-tracking/homework/train.py
+++ cohorts/2025/02-experiment-tracking/homework/train.py
@@ -1,9 +1,13 @@
 import os
 import pickle
 import click
+import mlflow
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-experiment")
+# mlflow ui --backend-store-uri sqlite:///mlflow.db
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 
 def load_pickle(filename: str):
@@ -17,16 +21,20 @@ def load_pickle(filename: str):
     default="./output",
     help="Location where the processed NYC taxi trip data was saved"
 )
+
 def run_train(data_path: str):
+    mlflow.autolog()
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+    mlflow.set_tag("developer", "Ivy")
 
     rf = RandomForestRegressor(max_depth=10, random_state=0)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+    rmse = root_mean_squared_error(y_val, y_pred)
 
 
 if __name__ == '__main__':
